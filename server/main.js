0a33697a21fa451677c2dc067a95495888cb69af
Athman Gude <athmangude@gmail.com>
2016-01-12 08:07:59 +0300

    set up web sockets with socket.io

--- server/main.js
+++ server/main.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(request, response) {
     response.send('Hello World');
     console.log('something connected to express');
+});
+
+io.on('connection', function(socket) {
+    console.log('something connected to Socket.io');
+    socket.emit('messages', ['Hello', 'Its me', 'Ive been wondering if we could meet again after such a long time']);
 });
 
 server.listen('8080');
