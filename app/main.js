379c3619dbc093d21a9e5e120906dc28494cb0d3
Athman Gude <athmangude@gmail.com>
2016-01-12 09:00:54 +0300

    preparing template string for html formatted messages

--- app/main.js
+++ app/main.js
@@ -2,4 +2,15 @@ var socket = io.connect('http://localhost:8080', {forceNew: true});
 
 socket.on('messages', function(data) {
     console.info(data);
+
+    var html = data.map(function(data) {
+        return (`
+            <div class="name">
+                ${data.userName}
+            </div>
+            <a href=${data.content.link} class="message" target="_blank">${data.content.text}</a>
+        `);
+    }).join('');
+
+    document.getElementById('messages').innerHTML = html;
 });
